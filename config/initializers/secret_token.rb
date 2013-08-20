51dd3881fe43a4cc8696976e32085c5d487b250e
jonny kang <jono.kang@gmail.com>
2013-08-20 16:47:25 -0700

    intitialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aboozt::Application.config.secret_key_base = 'fcfff7acb5f22604b50508c043b9936d2b8ac9ac2e93ae930f5fc72589eb66563afe3958b02955914e8ffced11a0d22ad823d3c20f0949b214ebc99c60cfa173'
